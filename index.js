fd43208845424fb73ae7a3b2c234b54dd4f98361
Ali <syedsumairali9007@gamil.com>
2022-11-21 18:01:19 +0530

    hello how are you

---
+++ index.js
@@ -0,0 +1,11 @@
+const search= document.querySelector("[data-search]")
+
+search.addEventListener("input",e=>{
+ const value = e.target.value
+ console.log(value)
+})
+
+const user=document.querySelector("[data-person]")
+console.log(user)
+
+
