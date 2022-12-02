93e59b71641154c23bbd7e5535edf50c7a763716
Ali <syedsumairali9007@gamil.com>
2022-12-02 19:24:43 +0530

    hello

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ syed = {
     sno: 2,
     dev: true
 }
-
 module.exports = syed;
+console.log(exports, require, __filename, __dirname)
+// hello = {
+//     name: "hi",
+//     sno: 1,
+//     dev: true
+// }
+// module.exports = hello;
