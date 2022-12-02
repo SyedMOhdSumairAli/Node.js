81e4b70e09afc188b9d81fd41fe5baa99ec0d1e0
Ali <syedsumairali9007@gamil.com>
2022-12-02 19:46:41 +0530

    hello this out us

---
+++ path.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const a1 = path.basename('C:\\temp\\myfile.html');
+const a2 = path.dirname('C:\\temp\\myfile.html');
+const a3 = path.extname(__filename);
+console.log(a3)
