00a6f34bed33a0b1302bf8e06607045553d6c5ae
Ali <syedsumairali9007@gamil.com>
2022-12-03 13:50:35 +0530

    commit it

---
+++ fs.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+// fs.readFilen('file.txt', 'utf8', (err, data) => {
+//     console.log(err, data)
+// })
+// const a = fs.readFileSync('file.txt')
+// console.log(a.toString())
+fs.writeFile('file2.hmtl', 'hello world',()=> {
+    console.log("file have")
+})
