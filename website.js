de89bc17e4e1bda6afdadfc3fe5f8dc2f0aa9eb2
Ali <syedsumairali9007@gamil.com>
2022-12-06 10:04:11 +0530

    jjjfsjfj

--- website.js
+++ website.js
@@ -1,14 +1,20 @@
 const http = require('http');
+const fs = require('fs')
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.setHeader = ('Content-Type', "text/html")
     console.log(req.url)
-    if (req.url == '/') {
+    if (req.url == '/link') {
         res.statusCode = 200;
         res.end('<h1>this is work sever from my system</h1><p>lorem this this that that when a;fjdkjf sdjfdsj </p>');
     } else if (req.url == '/about') {
         res.statusCode = 200;
         res.end('<h1>this is work sever from my system</h1><p>lorem this this that that when a;fjdkjf sdjfdsj </p>');
+    }
+    else if (req.url == '/') {
+        res.statusCode = 200;
+        const data = fs.readFileSync("file2.html")
+        res.end(data.toString());
     } else {
         res.statusCode = 404;
         res.end('<h1>Not Found</h1>');
